520335d8b230a43d704a0d3e53f79091047d7744
nobbaggu <jchg1147@gmail.com>
2020-07-13 01:01:05 +0900

    기본 권한 GUEST -> USER 변경

--- src/main/java/com/changgeol/www/springboot/Application.java
+++ src/main/java/com/changgeol/www/springboot/Application.java
@@ -2,8 +2,6 @@ package com.changgeol.www.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
 //@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
